ce7bbdbe807992e3d4d1378ae3eb58fa513ab1ff
Matheus <matheus.physicist369@gmail.com>
2024-10-22 11:25:28 -0300

    Atividade

---
+++ projetoTesteHospede/src/main/java/com/teste/hospede/repository/VeiculosRepository.java
@@ -0,0 +1,13 @@
+package com.teste.hospede.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.teste.hospede.entity.Veiculos;
+
+
+
+
+
+public interface VeiculosRepository extends JpaRepository<Veiculos, Long>{
+
+}
